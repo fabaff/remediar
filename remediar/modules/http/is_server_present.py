44b462668c3cf351318b7606c0f1d1e8e2fcb458
Fabian Affolter <mail@fabian-affolter.ch>
2019-07-02 21:16:37 +0200

    Add HTTP header server entry

---
+++ remediar/modules/http/is_server_present.py
@@ -0,0 +1,56 @@
+"""Support for getting the service banner of a web server."""
+import requests
+import urllib3
+
+from remediar.helper import Check
+
+urllib3.disable_warnings()
+
+
+class CheckHttpIsServerPresent(Check):
+    """Representation of a HTTP banner check."""
+
+    def __init__(self, server):
+        """Initialize the check."""
+        self._server = server
+        self._name = ""
+        self._output = None
+        self.run_check()
+
+    @property
+    def name(self) -> str:
+        """Return the name of the check."""
+        return self._name
+
+    @property
+    def result(self) -> bool:
+        """Return the state of the check."""
+        if self._output is not False and self._output is not None:
+            return True
+        else:
+            return False
+
+    @property
+    def output(self) -> str:
+        """Return the output of the check."""
+        return self._output
+
+    def run_check(self):
+        """Run the check."""
+        url = "http://{}".format(self._server)
+
+        try:
+            response = requests.head(
+                url, allow_redirects=False, verify=False, timeout=5
+            )
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.MissingSchema,
+        ):
+            self._output = None
+            return
+
+        if "Server" in response.headers:
+            self._output = response.headers["Server"]
+        else:
+            self._output = False
