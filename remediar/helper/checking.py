872bf6e7de75c7b97bd6e17597266baa976d80e4
Fabian Affolter <mail@fabian-affolter.ch>
2019-07-08 20:19:01 +0200

    Fix style

--- remediar/helper/checking.py
+++ remediar/helper/checking.py
@@ -5,10 +5,11 @@ import importlib
 def get_check(check_group, check_type):
     """Get the check class."""
     check_module = importlib.import_module(
-        "remediar.modules.{}.{}".format(check_group, check_type))
+        "remediar.modules.{}.{}".format(check_group, check_type)
+    )
     check_class_name = "Check{}{}".format(
-        check_group.capitalize(), check_type.title().replace('_', ''))
+        check_group.capitalize(), check_type.title().replace("_", "")
+    )
     CheckClass = getattr(check_module, check_class_name)
 
     return CheckClass
-
--- remediar/helper/output.py
+++ remediar/helper/output.py
@@ -3,7 +3,7 @@
 
 def add_row(rows, target, group, check, result):
     """Format a single row for the output."""
-    _result = '1' if result.result is True else '0'
-    output = '-' if result.output is False else result.output
+    _result = "1" if result.result is True else "0"
+    output = "-" if result.output is False else result.output
 
     rows.append([target, group, check, _result, output])
