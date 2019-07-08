04d410eb2216c403eaba725f0fc8e46af5cb7567
Fabian Affolter <mail@fabian-affolter.ch>
2019-07-09 00:00:30 +0200

    Detect check group automatically

--- remediar/controllers/base.py
+++ remediar/controllers/base.py
@@ -1,11 +1,12 @@
 """Base part of the remediar CLI."""
-from cement import Controller, ex
-import os
-import yaml
-from ..helper.checking import get_check
 import datetime
+import os
 
-from ..constants import RUN_BANNER, VERSION_BANNER, HEADERS
+from cement import Controller, ex
+import yaml
+
+from ..constants import HEADERS, RUN_BANNER, VERSION_BANNER
+from ..helper.checking import get_check
 from ..helper.output import add_row
 
 
@@ -14,13 +15,14 @@ class Base(Controller):
 
     class Meta:
         """Meta class for the CLI."""
+
         label = "base"
 
         # Text displayed at the top of --help output
         description = "Remediar is an issue and vulnerability tracker framework"
 
         # Text displayed at the bottom of --help output
-        #epilog = "Usage: remediar run --foo bar"
+        # epilog = "Usage: remediar run --foo bar"
 
         # Controller level arguments. ex: 'remediar --version'
         arguments = [
@@ -32,10 +34,7 @@ class Base(Controller):
         """Default action if no sub-command is passed."""
         self.app.args.print_help()
 
-    @ex(
-        help="run tasks",
-        arguments=[(["file"], {"help": "tasks file to process"})],
-    )
+    @ex(help="run tasks", arguments=[(["file"], {"help": "tasks file to process"})])
     def run(self):
         """Run sub-command."""
         print(RUN_BANNER)
@@ -44,60 +43,37 @@ class Base(Controller):
 
         tasks_file_path = os.getcwd()
 
-        with open(os.path.join(tasks_file_path, tasks_file), encoding='utf-8') as desc:
+        with open(os.path.join(tasks_file_path, tasks_file), encoding="utf-8") as desc:
             tasks = desc.read()
 
         data = yaml.safe_load(tasks)
 
-        self.app.print("{} - {} ({})".format(
-            data.get('name'), data.get('description'), datetime.datetime.now().isoformat()
-        ))
+        self.app.print(
+            "{} - {} ({})".format(
+                data.get("name"),
+                data.get("description"),
+                datetime.datetime.now().isoformat(),
+            )
+        )
 
         rows = []
 
-        for host in data['hosts']:
-            #self.app.print((host['ip_address']))
-            target = host['ip_address']
+        for host in data["hosts"]:
+            target = host["ip_address"]
 
-            # HTTP
-            if host.get('http', None) is not None:
-                http_config = host['http']
-                port = http_config.get('port', 80)
-                checks = http_config.get('checks')
+            groups = list(host.keys())
+            groups.remove("ip_address")
+            groups.remove("name")
 
-                for check in checks:
-                    result = get_check('http', check)(target)
-                    add_row(rows, target, 'http', check, result)
-
-            # telnet
-            if host.get('telnet', None) is not None:
-                telnet_config = host['telnet']
-                checks = telnet_config.get('checks')
+            for group in groups:
+                group_config = host[group]
+                checks = group_config.get("checks")
 
                 for check in checks:
-                    result = get_check('telnet', check)(target)
-                    add_row(rows, target, 'telnet', check, result)
-
-            # SSH
-            if host.get('ssh', None) is not None:
-                ssh_config = host['ssh']
-                port = ssh_config.get('port', 22)
-
-                checks = ssh_config.get('checks')
-
-                for check in checks:
-                    result = get_check('ssh', check)(target, port)
-                    add_row(rows, target, 'ssh', check, result)
-
-            #    TCP
-            if host.get('tcp', None) is not None:
-                tcp_config = host['tcp']
-                ports = tcp_config.get('ports')
-                checks = tcp_config.get('checks')
-
-                for check in checks:
-                    for port in ports:
-                        result = get_check('tcp', check)(target, port)
-                        add_row(rows, target, 'tcp', check, result)
+                    self.app.print(
+                        "Running {} of {} on {}".format(check, group, target)
+                    )
+                    result = get_check(group, check)(target)
+                    add_row(rows, target, group, check, result)
 
         self.app.render(rows, headers=HEADERS)
